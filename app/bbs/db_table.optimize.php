baafcf5442d9e975dce44b2b0d141aae2fa9ab7e
kagla <dnaustks1@gmail.com>
2026-05-05 07:21:04 +0900

    sql: bbs/db_table.optimize.php raw → named PDO (DB 자동 청소 + OPTIMIZE TABLE 유지)

--- app/bbs/db_table.optimize.php
+++ app/bbs/db_table.optimize.php
@@ -12,49 +12,49 @@ if(isset($config['cf_optimize_date']) && $config['cf_optimize_date'] >= G5_TIME_
 // 설정일이 지난 접속자로그 삭제
 if($config['cf_visit_del'] > 0) {
     $tmp_before_date = date("Y-m-d", G5_SERVER_TIME - ($config['cf_visit_del'] * 86400));
-    $sql = " delete from {$g5['visit_table']} where vi_date < '$tmp_before_date' ";
-    sql_query($sql);
+    sql_pdo_query(" delete from {$g5['visit_table']} where vi_date < :before_date ",
+                  [':before_date' => $tmp_before_date]);
     if (defined('G5_USE_OPTIMIZE_DBTABLE') && G5_USE_OPTIMIZE_DBTABLE) {
-        sql_query(" OPTIMIZE TABLE `{$g5['visit_table']}`, `{$g5['visit_sum_table']}` ");
+        sql_pdo_query(" OPTIMIZE TABLE `{$g5['visit_table']}`, `{$g5['visit_sum_table']}` ");
     }
 }
 
 // 설정일이 지난 인기검색어 삭제
 if($config['cf_popular_del'] > 0) {
     $tmp_before_date = date("Y-m-d", G5_SERVER_TIME - ($config['cf_popular_del'] * 86400));
-    $sql = " delete from {$g5['popular_table']} where pp_date < '$tmp_before_date' ";
-    sql_query($sql);
+    sql_pdo_query(" delete from {$g5['popular_table']} where pp_date < :before_date ",
+                  [':before_date' => $tmp_before_date]);
     if (defined('G5_USE_OPTIMIZE_DBTABLE') && G5_USE_OPTIMIZE_DBTABLE) {
-        sql_query(" OPTIMIZE TABLE `{$g5['popular_table']}` ");
+        sql_pdo_query(" OPTIMIZE TABLE `{$g5['popular_table']}` ");
     }
 }
 
 // 설정일이 지난 최근게시물 삭제
 if($config['cf_new_del'] > 0) {
-    $sql = " delete from {$g5['board_new_table']} where (TO_DAYS('".G5_TIME_YMDHIS."') - TO_DAYS(bn_datetime)) > '{$config['cf_new_del']}' ";
-    sql_query($sql);
-    
+    sql_pdo_query(" delete from {$g5['board_new_table']} where (TO_DAYS(:now) - TO_DAYS(bn_datetime)) > :days ",
+                  [':now' => G5_TIME_YMDHIS, ':days' => (int) $config['cf_new_del']]);
+
     // 데이터가 많으면 처리시간 때문에 브라우저 응답이 늦을수 있음
     if (defined('G5_USE_OPTIMIZE_DBTABLE') && G5_USE_OPTIMIZE_DBTABLE) {
-        sql_query(" OPTIMIZE TABLE `{$g5['board_new_table']}` ");
+        sql_pdo_query(" OPTIMIZE TABLE `{$g5['board_new_table']}` ");
     }
 }
 
 // 설정일이 지난 쪽지 삭제
 if($config['cf_memo_del'] > 0) {
-    $sql = " delete from {$g5['memo_table']} where (TO_DAYS('".G5_TIME_YMDHIS."') - TO_DAYS(me_send_datetime)) > '{$config['cf_memo_del']}' ";
-    sql_query($sql);
+    sql_pdo_query(" delete from {$g5['memo_table']} where (TO_DAYS(:now) - TO_DAYS(me_send_datetime)) > :days ",
+                  [':now' => G5_TIME_YMDHIS, ':days' => (int) $config['cf_memo_del']]);
     if (defined('G5_USE_OPTIMIZE_DBTABLE') && G5_USE_OPTIMIZE_DBTABLE) {
-        sql_query(" OPTIMIZE TABLE `{$g5['memo_table']}` ");
+        sql_pdo_query(" OPTIMIZE TABLE `{$g5['memo_table']}` ");
     }
 }
 
 // 탈퇴회원 자동 삭제
 if($config['cf_leave_day'] > 0) {
-    $sql = " select mb_id from {$g5['member_table']}
-                where (TO_DAYS('".G5_TIME_YMDHIS."') - TO_DAYS(mb_leave_date)) > '{$config['cf_leave_day']}'
-                  and mb_memo not regexp '^[0-9]{8}.*삭제함' ";
-    $result = sql_query($sql);
+    $result = sql_pdo_query(" select mb_id from {$g5['member_table']}
+                where (TO_DAYS(:now) - TO_DAYS(mb_leave_date)) > :days
+                  and mb_memo not regexp '^[0-9]{8}.*삭제함' ",
+                [':now' => G5_TIME_YMDHIS, ':days' => (int) $config['cf_leave_day']]);
     while ($row=sql_fetch_array($result))
     {
         // 회원자료 삭제
@@ -74,8 +74,9 @@ if($captcha_mp3 && is_array($captcha_mp3)) {
 
 // 실행일 기록
 if(isset($config['cf_optimize_date'])) {
-    sql_query(" update {$g5['config_table']} set cf_optimize_date = '".G5_TIME_YMD."' ");
-    
+    sql_pdo_query(" update {$g5['config_table']} set cf_optimize_date = :ymd ",
+                  [':ymd' => G5_TIME_YMD]);
+
     run_event('cf_optimize_date_update', $config);
 }
 
